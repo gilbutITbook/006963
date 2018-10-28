95c01321bcd7c11ec7505342f8f5517ee700e2f0
Donga Kim <dongakim90@gmail.com>
2018-10-28 19:33:21 +0900

    책에 나와있는 예제 코드 추가
    책 97쪽에 있는 예제코드 내용을 추가하였습니다.

--- ch04/check-hand-with-mocha-7.js
+++ ch04/check-hand-with-mocha-7.js
@@ -17,6 +17,24 @@ function multiplesIn(hand){
  return highestCount(valuesFromHand(hand));
 };
 
+function highestCount(values) {};
+
+function valuesFromHand(hand) {};
+
+describe('valuesFromHand()', function() {
+ it('returns just the values from a hand', function() {
+  var result = valuesFromHand(['2-H', '3-C', '4-D', '5-H', '2-C']);
+  wish(result === ['2', '3', '4', '5', '2']);
+ }); });
+
+describe('highestCount()', function() {
+ it('returns count of the most common card from array',
+  function() {
+   var result = highestCount(['2', '4', '4', '4', '2']);
+   wish(result === 3);
+  }
+ );
+});
 
 describe('multiplesIn()', function() {
  it('finds a duplicate', function() {
